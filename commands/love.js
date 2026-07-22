aa2cc5f04b362a1b3bdaeef30d4171b872192766
testtest0192ow-rgb <testtest0192ow@gmail.com>
2026-07-22 14:04:15 +0300

    Update love.js

--- commands/love.js
+++ commands/love.js
@@ -1,1 +1,99 @@
+const {
+    SlashCommandBuilder,
+    AttachmentBuilder
+} = require("discord.js");
 
+const createLoveCard = require("../utils/loveCard");
+
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+
+        .setName("love")
+
+        .setDescription("Показать профиль любви"),
+
+
+
+    async execute(interaction) {
+
+
+        await interaction.deferReply();
+
+
+
+        const user = interaction.user;
+
+
+
+        // временные данные
+        // потом заменим на MongoDB
+
+        const loveData = {
+
+
+            username: user.username,
+
+
+            avatar: user.displayAvatarURL({
+
+                extension: "png",
+
+                size: 512
+
+            }),
+
+
+            partner: "Нет пары",
+
+
+            status: "Свободен",
+
+
+            startDate: "—",
+
+
+            days: 0,
+
+
+            loveLevel: 1,
+
+
+            loveXp: 0
+
+
+        };
+
+
+
+        const image = await createLoveCard(
+            loveData
+        );
+
+
+
+        const file = new AttachmentBuilder(
+
+            image,
+
+            {
+                name: "love-profile.png"
+            }
+
+        );
+
+
+
+        await interaction.editReply({
+
+            files: [
+                file
+            ]
+
+        });
+
+
+    }
+
+};
