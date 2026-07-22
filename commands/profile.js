deae9646cab6e83cddbe2456992d435969d8acdb
testtest0192ow-rgb <testtest0192ow@gmail.com>
2026-07-22 13:31:29 +0300

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -1,94 +1,136 @@
 const {
+
     SlashCommandBuilder,
+
     AttachmentBuilder,
+
     ActionRowBuilder,
+
     ButtonBuilder,
+
     ButtonStyle
+
 } = require("discord.js");
+
 
 const createProfileCard = require("../utils/profileCard");
 
 
+
 module.exports = {
 
-    data: new SlashCommandBuilder()
-        .setName("profile")
-        .setDescription("Показать профиль пользователя"),
 
+data: new SlashCommandBuilder()
 
-    async execute(interaction) {
+.setName("profile")
 
-        const user = interaction.user;
-
-
-        const image = await createProfileCard({
-
-            username: user.username,
-
-            avatar: user.displayAvatarURL({
-                extension: "png",
-                size: 256
-            }),
-
-
-            joinDate: "22.07.26",
-
-
-            balance: "5 000 000",
-
-            // текущий стрик пользователя
-            streak: 25,
-
-
-            marriage: "Нет",
-
-
-            voice: 240,
-
-
-            level: 15
-
-        });
+.setDescription("Показать профиль пользователя"),
 
 
 
-        const file = new AttachmentBuilder(
-            image,
-            {
-                name: "profile.png"
-            }
-        );
+async execute(interaction){
 
 
 
-        const buttons = new ActionRowBuilder()
-            .addComponents(
-
-                new ButtonBuilder()
-
-                    .setCustomId("love_profile")
-
-                    .setLabel("Профиль любви")
-
-                    // твой кастомный эмодзи любви
-                    .setEmoji("<:emoji_6:1529430913396506705>")
-
-                    .setStyle(ButtonStyle.Secondary)
-
-            );
+const user = interaction.user;
 
 
 
-        await interaction.reply({
+// временно, потом подключим MongoDB
 
-            files: [file],
+const userData = {
 
-            components: [
-                buttons
-            ]
 
-        });
+username: user.username,
 
-    }
+
+avatar: user.displayAvatarURL({
+
+extension:"png",
+
+size:256
+
+}),
+
+
+joinDate:"22.07.2026",
+
+
+balance:"5 000 000",
+
+
+streak:25,
+
+
+marriage:"Нет",
+
+
+voice:240,
+
+
+messages:"12 500",
+
+
+level:15
+
+};
+
+
+
+const image = await createProfileCard(userData);
+
+
+
+const file = new AttachmentBuilder(
+
+image,
+
+{
+
+name:"profile.png"
+
+}
+
+);
+
+
+
+
+
+const button = new ActionRowBuilder()
+
+.addComponents(
+
+
+new ButtonBuilder()
+
+.setCustomId("love_profile")
+
+.setLabel("Профиль любви")
+
+.setEmoji("<:emoji_6:1529430913396506705>")
+
+.setStyle(ButtonStyle.Secondary)
+
+
+
+);
+
+
+
+
+
+await interaction.reply({
+
+files:[file],
+
+components:[button]
+
+});
+
+
+
+}
+
 
 };
