af5da6177b636a3adfe556acd420ed409b8a12df
testtest0192ow-rgb <testtest0192ow@gmail.com>
2026-07-22 09:20:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,72 @@
 require("dotenv").config();
 
-const { Client, GatewayIntentBits } = require("discord.js");
+const { 
+    Client, 
+    GatewayIntentBits,
+    REST,
+    Routes,
+    SlashCommandBuilder
+} = require("discord.js");
+
 
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.Guilds
     ]
 });
 
 
-client.once("ready", () => {
-    console.log(`✅ WERTIX запущен как ${client.user.tag}`);
+const commands = [
+    new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Проверка работы WERTIX")
+].map(command => command.toJSON());
+
+
+const rest = new REST({ version: "10" })
+    .setToken(process.env.TOKEN);
+
+
+client.once("ready", async () => {
+
+    console.log(`✅ WERTIX онлайн: ${client.user.tag}`);
+
+
+    try {
+
+        await rest.put(
+            Routes.applicationCommands(client.user.id),
+            {
+                body: commands
+            }
+        );
+
+
+        console.log("✅ Команды загружены");
+
+    } catch(error) {
+
+        console.log(error);
+
+    }
+
+});
+
+
+client.on("interactionCreate", async interaction => {
+
+    if (!interaction.isChatInputCommand()) return;
+
+
+    if (interaction.commandName === "ping") {
+
+        await interaction.reply({
+            content: "🏓 WERTIX работает!",
+            ephemeral: false
+        });
+
+    }
+
 });
 
 
