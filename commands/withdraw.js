927030dac833b5fd9ef972e400bcf75d05cac303
testtest0192ow-rgb <testtest0192ow@gmail.com>
2026-08-01 06:46:09 +0300

    Update withdraw.js

--- commands/withdraw.js
+++ commands/withdraw.js
@@ -5,54 +5,24 @@ const emojis = require('../utils/emojis');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('withdraw')
-        .setDescription('Снять деньги из банка')
-        .addIntegerOption(option =>
-            option.setName('amount')
-                .setDescription('Сколько снять')
-                .setRequired(true)
+        .setDescription('Снять деньги')
+        .addIntegerOption(opt =>
+            opt.setName('amount').setRequired(true)
         ),
 
     async execute(interaction) {
         const amount = interaction.options.getInteger('amount');
 
-        if (amount <= 0) {
-            return interaction.reply({
-                content: `${emojis.error} Укажи норм сумму`,
-                ephemeral: true
-            });
-        }
-
         let user = await User.findOne({ userId: interaction.user.id });
 
-        if (!user) {
-            return interaction.reply({
-                content: `${emojis.error} У тебя нет аккаунта`,
-                ephemeral: true
-            });
-        }
-
-        if (user.bank < amount) {
-            return interaction.reply({
-                content: `${emojis.error} Недостаточно в банке`,
-                ephemeral: true
-            });
+        if (!user || user.bank < amount || amount <= 0) {
+            return interaction.reply(`${emojis.error} Ошибка`);
         }
 
         user.bank -= amount;
         user.balance += amount;
-
         await user.save();
 
-        const embed = new EmbedBuilder()
-            .setColor('#2f3136')
-            .setTitle(`${emojis.bank} Снятие`)
-            .setDescription(
-                `${emojis.money} Ты снял **${amount}** монет\n\n` +
-                `${emojis.wallet} Кошелёк: **${user.balance}**\n` +
-                `${emojis.bank} Банк: **${user.bank}**`
-            )
-            .setTimestamp();
-
-        interaction.reply({ embeds: [embed] });
+        interaction.reply(`${emojis.success} Ты снял ${amount}`);
     }
 };
